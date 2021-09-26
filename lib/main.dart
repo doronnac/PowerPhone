5f15b580c5e7b83881d35b05e6ad39ff6b3add9e
Doron Nachshon <doron6387@gmail.com>
2021-09-26 15:37:02 +0300

    sample contactlist project

--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,31 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:permission_handler/permission_handler.dart';
 
-void main() => runApp(const MyApp());
+import './pages/home_page.dart';
 
-Future<void> k() async {
-  if (await Permission.contacts.request().isGranted) {
-    print('GRANTED');
-  } else {
-    print('NOT GRANTED');
-  }
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    k();
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: const RandomWords(),
+      title: 'Simple Flutter Contact List',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.white,
-      )
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
+        scaffoldBackgroundColor: Colors.grey[900],
+        brightness: Brightness.dark,
+        primaryColor: Colors.green,
+        accentColor: Colors.greenAccent,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 24, color: Colors.white),
+          headline6: TextStyle(fontSize: 16, color: Colors.green),
+        ),
       ),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(18.0),
-        itemBuilder: (context, i) {
-          if (i >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[i]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue[700],
-        selectedFontSize: 13,
-        unselectedFontSize: 13,
-        iconSize: 30,
-        items: const [
-          BottomNavigationBarItem(
-            label: "Home",
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            label: "Favorites",
-            icon: Icon(Icons.star),
-          ),
-        ],
-      ),
+      home: MyHomePage(title: 'Simple Flutter Contact List'),
     );
   }
 }
